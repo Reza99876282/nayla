a8f419cae32985795ead543bf51362bb623c3644
Reza99876282 <119847616+Reza99876282@users.noreply.github.com>
2022-12-05 15:56:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ let chalk = require('chalk')
 
 global.Owner = {
     // use the phone number with the country code, for example Indonesia '62' America '1' etc
-    '628818770766': { // 628818770766
-        name: 'Zevano',
+    '6283164909633': { // 6283164909633
+        name: 'RezaPhLvi',
         isReport: true, // if true this number will send if user use command /report or /join
         isCreator: true, // if true this number will send if user use command /owner or /creator
         isDev: true
@@ -12,19 +12,19 @@ global.Owner = {
 }
 global.ext = {
 	title: 'Sabar sedang di proses',
-	body: 'NayLa-B0TZ',
+	body: 'Yuji-B0TZ',
 	thum: 'https://telegra.ph/file/5859353a380d9b67be198.jpg'
 }
-global.mods = ['6283897390164'] // Want some help?
+global.mods = ['6281367215117'] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Premium user has unlimited limit
 global.DeveloperMode = 'false' // true or false
 global.linkGC = ['https://chat.whatsapp.com/JgFOfu182li7kG6dlKcwzD']
-global.packname = 'NayLa-B0TZ'
-global.author = 'Zevano'
+global.packname = 'Yuji-B0TZ'
+global.author = 'RezaPhLvi'
 global.multiplier = 36 // The higher, The harder levelup
-global.sessionName = 'nayla' // The Session name behind the .json example is nayla.json
+global.sessionName = 'Yuji' // The Session name behind the .json example is nayla.json
 global.mesreport = 50
-global.botname = 'NAYLA-BOTZ'
+global.botname = 'Yuji-BOTZ'
 global.thumnail = ext.thum // It's hard to change if you don't get an error
 global.thum = ext.thum // Same as above
     global.mess = {
